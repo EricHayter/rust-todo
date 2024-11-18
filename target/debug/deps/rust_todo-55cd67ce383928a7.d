e939bba291f38cd53d31f8b10385ce4b387f4981
Eric <hayter.eric@gmail.com>
2024-11-18 15:24:38 -0500

    initial commit

---
+++ target/debug/deps/rust_todo-55cd67ce383928a7.d
@@ -0,0 +1,5 @@
+/home/eric/dev/rust-todo/target/debug/deps/rust_todo-55cd67ce383928a7: src/main.rs
+
+/home/eric/dev/rust-todo/target/debug/deps/rust_todo-55cd67ce383928a7.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-todo.d
@@ -0,0 +1,1 @@
+/home/eric/dev/rust-todo/target/debug/rust-todo: /home/eric/dev/rust-todo/src/main.rs
